a8539fdc53aaa1e776d4bf5ce78296a5f9cdc553
Ivan Karavai <ikaravai@solvd.com>
2022-07-28 12:43:47 +0300

    First commit

---
+++ license-service/src/test/java/com/solvd/ikaravai/licenseservice/LicenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solvd.ikaravai.licenseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
