be0e1ce791df6e0a07375b286a4e0de047a7beeb
Ivan Karavai <ikaravai@solvd.com>
2022-08-09 13:06:18 +0300

    Implement zipkin with ELK, custom spans

--- license-service/src/main/java/com/solvd/ikaravai/licenseservice/service/client/OrganizationRestTemplateClient.java
+++ license-service/src/main/java/com/solvd/ikaravai/licenseservice/service/client/OrganizationRestTemplateClient.java
@@ -1,16 +1,16 @@
 package com.solvd.ikaravai.licenseservice.service.client;
 
+import brave.ScopedSpan;
+import brave.Tracer;
 import com.solvd.ikaravai.licenseservice.model.Organization;
 import com.solvd.ikaravai.licenseservice.repository.OrganizationRedisRepository;
 import com.solvd.ikaravai.licenseservice.utils.UserContext;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 @RequiredArgsConstructor
@@ -19,6 +19,7 @@ public class OrganizationRestTemplateClient {
 
     private final KeycloakRestTemplate restTemplate;
     private final OrganizationRedisRepository organizationRedisRepository;
+    private final Tracer tracer;
 
     public Organization getOrganization(String organizationId) {
         log.info("Im license service.getOrganization: {}", UserContext.getCorrelationId());
@@ -43,11 +44,17 @@ public class OrganizationRestTemplateClient {
     }
 
     private Organization checkRedisCache(String organizationId) {
+        ScopedSpan newSpan = tracer.startScopedSpan("readLicensingDataFromRedis");
         try {
             return organizationRedisRepository.findById(organizationId).orElse(null);
         } catch (Exception ex){
             log.error("Error encountered while trying to retrieve organization {} check Redis Cache.  Exception {}", organizationId, ex);
             return null;
+        } finally {
+            log.info("CLOSING REDIS SPAN ------------");
+            newSpan.tag("peer.service", "redis");
+            newSpan.annotate("Client received");
+            newSpan.finish();
         }
     }
 
--- organization-service/src/main/java/com/solvd/ikaravai/organizationservice/service/OrganizationService.java
+++ organization-service/src/main/java/com/solvd/ikaravai/organizationservice/service/OrganizationService.java
@@ -1,10 +1,13 @@
 package com.solvd.ikaravai.organizationservice.service;
 
+import brave.ScopedSpan;
+import brave.Tracer;
 import com.solvd.ikaravai.organizationservice.event.model.ActionEnum;
 import com.solvd.ikaravai.organizationservice.event.source.SimpleSourceBean;
 import com.solvd.ikaravai.organizationservice.model.Organization;
 import com.solvd.ikaravai.organizationservice.repository.OrganizationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,15 +15,34 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrganizationService {
 
     private final OrganizationRepository organizationRepository;
     private final SimpleSourceBean simpleSourceBean;
+    private final Tracer tracer;
 
     public Organization findById(String organizationId) {
-        Optional<Organization> org = organizationRepository.findById(organizationId);
-        simpleSourceBean.publishOrganizationChange(ActionEnum.GET, organizationId);
-        return org.orElse(null);
+//        Optional<Organization> org = organizationRepository.findById(organizationId);
+//        simpleSourceBean.publishOrganizationChange(ActionEnum.GET, organizationId);
+//        return org.orElse(null);
+        Optional<Organization> opt = null;
+        ScopedSpan newSpan = tracer.startScopedSpan("getOrgDBCall");
+        try {
+            opt = organizationRepository.findById(organizationId);
+            simpleSourceBean.publishOrganizationChange(ActionEnum.GET, organizationId);
+            if (!opt.isPresent()) {
+                String message = String.format("Unable to find an organization with the Organization id %s", organizationId);
+                log.error(message);
+                throw new IllegalArgumentException(message);
+            }
+            log.debug("Retrieving Organization Info: " + opt.get().toString());
+        }finally {
+            newSpan.tag("peer.service", "postgres");
+            newSpan.annotate("Client received");
+            newSpan.finish();
+        }
+        return opt.get();
     }
 
     public Organization create(Organization organization){
