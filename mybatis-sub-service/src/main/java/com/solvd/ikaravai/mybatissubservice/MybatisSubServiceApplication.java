220e1e4befef5b8fc887a7bbb0dd4305e1d3b708
Ivan Karavai <ikaravai@solvd.com>
2022-08-18 19:32:20 +0300

    Add subscriber service, RabbitMQ container

---
+++ mybatis-sub-service/src/main/java/com/solvd/ikaravai/mybatissubservice/MybatisSubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.solvd.ikaravai.mybatissubservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisSubServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisSubServiceApplication.class, args);
+    }
+
+}
---
+++ mybatis-sub-service/src/test/java/com/solvd/ikaravai/mybatissubservice/MybatisSubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solvd.ikaravai.mybatissubservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisSubServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
